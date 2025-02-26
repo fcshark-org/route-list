7504b6b2bc97076b77824b2c6a6612f0aa926fb3
Anya Lin <muink@users.noreply.github.com>
2025-02-26 09:41:29 +0800

    Update genlists.sh

--- genlists.sh
+++ genlists.sh
@@ -103,26 +103,32 @@ update_ipcidr() {
 	EOF
 
 	## Compile mihoho ruleset
-	[ "$(which mihomo 2>/dev/null)"x = 'x' ] || {
+	! [ -x "$(command -v mihomo)" ] || {
 		cat "$IPv4" "$IPv6" > mihomo.tmp
 		mihomo convert-ruleset ipcidr text mihomo.tmp china_ip.mrs
 	}
 
 	## Compile sing-box rule-set
-	[ "$(which sing-box 2>/dev/null)"x = 'x' ] || {
-		printf '{\n\t"version": 3,\n\t"rules": [\n' > china_ip.json
-		printf '\t\t{\n\t\t\t"ip_cidr": [\n' >> china_ip.json
-		while read -r cnip4; do
-			printf '\t\t\t\t"%s",\n' "$cnip4" >> china_ip.json
-		done < "$IPv4"
-		while read -r cnip6; do
-			printf '\t\t\t\t"%s",\n' "$cnip6" >> china_ip.json
-		done < "$IPv6"
-		$SED -i '$s/,//' china_ip.json
-		printf '\t\t\t]\n\t\t}\n' >> china_ip.json
-		printf '\t]\n}\n' >> china_ip.json
-		sing-box rule-set compile china_ip.json
-		rm china_ip.json
+	! [ -x "$(command -v sing-box)" ] || {
+		IPCIDR='china_ip.json'
+		cat <<-EOF > $IPCIDR
+			{
+			  "version": 3,
+			  "rules": [
+			    {
+			      "ip_cidr": [
+		EOF
+		$SED -En 's|^|        "|; s|$|",|; p' "$IPv4" >> $IPCIDR
+		$SED -En 's|^|        "|; s|$|",|; p' "$IPv6" >> $IPCIDR
+		$SED -i '${s|,$||}' $IPCIDR
+		cat <<-EOF >> $IPCIDR
+			      ]
+			    }
+			  ]
+			}
+		EOF
+		sing-box rule-set compile $IPCIDR
+		rm $IPCIDR
 	}
 
 	# Cleanup
